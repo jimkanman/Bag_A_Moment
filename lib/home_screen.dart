3ae4914d90c34d23b583a9ca305f577358d9549f
정해솔 <102032766+DOL-DOM@users.noreply.github.com>
2024-10-07 23:53:37 +0900

    로딩->1.에러 2. 정상 홈화면 구현

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 //홈화면 클래스 생성
 class HomeScreen extends StatelessWidget{
-  const HomeScreen (Key? key) : super(key: key);
+  const HomeScreen ({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context){
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomeScreen();
+    return FutureBuilder<Object>( //퓨처 빌더 위젯: 로딩-> 1. 에러 2. 메인 처리
+      future: Future.delayed(Duration(seconds:3), () => 100), //3초 후 빌더값은 스냅샷 불러옴
+      builder: (context, snapshot) {
+        return AnimatedSwitcher(
+          duration: Duration(milliseconds: 900), //페이드 인아웃 효과
+          child: _loadingWidget(snapshot), //로딩 위젯 지정
+        );
+      }
+    );
+  }
+  //지정 로딩위젯 선언
+  StatelessWidget _loadingWidget(AsyncSnapshot<Object> snapshot) {
+    if(snapshot.hasError) {print('로딩 중 에러가 발생하였습니다.'); return Text('Error');}
+    else if(snapshot.hasData){return HomeScreen();}
+    else{return loadingScreen();}
+
   }
 }
+//
