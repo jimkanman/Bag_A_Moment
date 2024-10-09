a9b8846ea0c0f1201299ef55a06337b6ee79bdad
정해솔 <102032766+DOL-DOM@users.noreply.github.com>
2024-10-09 16:57:35 +0900

    Google Maps API 설정-android, iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyD7FUQ1x1gATAYbzvXAi8JF7x-_jIHR09w") // 여기에 API 키 추가
+    GeneratedPluginRegistrant.register(with: self)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
