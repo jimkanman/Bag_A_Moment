c525b28a47545ddfceb2423584bd58403ff0a5ed
Miging <32047017+Miging@users.noreply.github.com>
2024-12-04 22:11:41 +0900

    AR 기능 부피 매핑 추가 (#10)
    * FEAT: 기존 메세지->Map으로 매핑
    
    * FEAT: 매핑된 변수 UI 구현

--- android/app/src/main/java/com/example/bag_a_moment/rawdepth/BridgeActivity.java
+++ android/app/src/main/java/com/example/bag_a_moment/rawdepth/BridgeActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
@@ -62,13 +65,16 @@ public class BridgeActivity extends FlutterActivity {
                 if (data == null) {
                     Log.e("Error", "data Intent is null");
                 } else {
-                    Log.d("Intent Data", "Width: " + data.getIntExtra("width", -1) +
-                            ", Height: " + data.getIntExtra("height", -1) +
-                            ", Depth: " + data.getIntExtra("depth", -1));
+                    int width = data.getIntExtra("width", -1);
+                    int height = data.getIntExtra("height", -1);
+                    int depth = data.getIntExtra("depth", -1);
+                    Map<String, Integer> resultMap = new HashMap<>();
+                    resultMap.put("width", width);
+                    resultMap.put("height", height);
+                    resultMap.put("depth", depth);
+
+                    myResult.success(resultMap);
                 }
-                myResult.success("Width: " + data.getIntExtra("width", -1) +
-                        ", Height: " + data.getIntExtra("height", -1) +
-                        ", Depth: " + data.getIntExtra("depth", -1));
             } else {
                 myResult.error("Unavailable", "짐 크기 정보 획득에 실패", null);
             }
