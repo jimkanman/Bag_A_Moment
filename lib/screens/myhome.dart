be23d366f4dab0325b91a42138162e25bb73a24d
SongMinhyuk <dnfldpden32@gmail.com>
2024-12-05 16:58:11 +0900

    FEAT: 마이페이지 기존 UI 변경

--- lib/screens/myhome.dart
+++ lib/screens/myhome.dart
@@ -6,22 +6,20 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:bag_a_moment/screens/mypage.dart'; // 회원 정보 조회 페이지
 import 'package:bag_a_moment/userInfo.dart';
 
-
 class MyPageMainScreen extends StatefulWidget {
   @override
-  _MyPageMainScreenState createState() =>  _MyPageMainScreenState();
+  _MyPageMainScreenState createState() => _MyPageMainScreenState();
 }
-
 
 class _MyPageMainScreenState extends State<MyPageMainScreen> {
   Map<String, dynamic>? _userData;
   Map<String, int> _volumeData = {};
-  static const platform=MethodChannel("com.example.example/message");
+  static const platform = MethodChannel("com.example.example/message");
 
-  Future<void> _fetchVolumeData() async{
+  Future<void> _fetchVolumeData() async {
     try {
-      final Map<dynamic, dynamic> result = await platform.invokeMethod(
-          'getVolumeAndroid');
+      final Map<dynamic, dynamic> result =
+          await platform.invokeMethod('getVolumeAndroid');
       setState(() {
         _volumeData = {
           'width': result['width'],
@@ -56,86 +54,42 @@ class _MyPageMainScreenState extends State<MyPageMainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.png'), // 배경 이미지 경로
-            fit: BoxFit.cover, // 화면에 맞게 이미지 크기 조정
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 16),
-                  Text(
-                    "${_userData!['nickname']}님,",
-                    style: TextStyle(
-                      fontSize: 38,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    "짐깐만요!",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+        backgroundColor: Color(0xffF7F8FA),
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(
+              top: 48,
+              left: 12,
+              right: 12,
             ),
-            Divider(
-              thickness: 1.0,
-              height: 20,
-              color: Color(0xFFE0F7F5),
-            ),
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.all(16.0),
-                children: [
-                  _buildButton(
-                    context,
-                    "회원 정보 조회 하기",
-                        () => Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyPage()),
-                    ),
-                  ),
-                  _buildButton(context, "그동안 맡긴 기록 확인하기", () {
-                    // TODO: 기록 확인 페이지로 이동
-
-                  }),
-                  _buildButton(context, "짐 크기 확인하기",_fetchVolumeData),
-                  _volumeData.isNotEmpty
-                      ? Text('Width: ${_volumeData['width']}, Height: ${_volumeData['height']}, Depth: ${_volumeData['depth']}')
-                      : Text('No data available'),
-                  _buildButton(context, "결제 수단 등록", () {
-                    // TODO: 결제 수단 등록 페이지로 이동
-                  }),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: myhome_bar(),
+                ),
+                Flexible(
+                    fit: FlexFit.loose,
+                    child: userbox()
+                ),
+                Flexible(child: settingbox())
+              ],
+            )));
   }
 
   // 버튼 위젯 생성 함수
-  Widget _buildButton(BuildContext context, String title, VoidCallback onPressed) {
+  Widget _buildButton(
+      BuildContext context, String title, VoidCallback onPressed) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          foregroundColor: Color(0xFF26D1BA), backgroundColor: Color(0xFFE0F7F5), minimumSize: Size(double.infinity, 50), // 글씨 색상
+          foregroundColor: Color(0xFF26D1BA),
+          backgroundColor: Color(0xFFE0F7F5),
+          minimumSize: Size(double.infinity, 50),
+          // 글씨 색상
           side: BorderSide(color: Colors.white, width: 1.5), // 테두리
         ),
         onPressed: onPressed,
