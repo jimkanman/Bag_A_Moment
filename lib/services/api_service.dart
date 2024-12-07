4ba83db4605b9257d2a446eaf0ce499da813930f
Jaehwan Hyun <jec0101@naver.com>
2024-12-07 18:48:33 +0900

    update: ApiService 생성 시 defaultHeader 부여 가능하도록 변경

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,9 +3,12 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   final String BASE_URL = "http://3.35.175.114:8080";
+  Map<String, String>? _defaultHeader;
 
-  ApiService();
+  ApiService({Map<String, String>? defaultHeader}):
+        _defaultHeader = defaultHeader;
 
+  /// 주어진 파라미터로 GET 전송 + data 필드에 fromJson 매핑하여 반환
   Future<T> get<T>(String endpoint, {Map<String, String>? headers, required T Function(dynamic) fromJson}) async {
     print("ApiService: GET to '$BASE_URL/$endpoint");
     final response = await http.get(Uri.parse('$BASE_URL/$endpoint'), headers: headers);
@@ -18,10 +21,12 @@ class ApiService {
     }
   }
 
+  /// 주어진 파라미터로 POST 전송 + data 필드에 fromJson 매핑하여 반환
   Future<T> post<T>(String endpoint, {Map<String, dynamic>? requestBody, Map<String, String>? headers, required T Function(dynamic) fromJson}) async {
-    print("ApiService: POST to /$endpoint with body=$requestBody");
+    print("ApiService: POST to '$_BASE_URL/$endpoint' with body=$requestBody");
     headers = {
       'Content-Type' : 'application/json',
+      ...?_defaultHeader,
       ...?headers
     };
     final response = await http.post(
