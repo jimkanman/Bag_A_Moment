7523989cde2365616fb8c70a1a1221d06cb34083
SongMinhyuk <dnfldpden32@gmail.com>
2024-12-08 00:57:12 +0900

    feat: textfield maxline 추가

--- lib/widgets/textfield.dart
+++ lib/widgets/textfield.dart
@@ -8,18 +8,21 @@ class CustomTextFormField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final String? Function(String?)? validator;
+  final int maxlines;
 
   const CustomTextFormField({
     super.key,
     required this.controller,
     required this.hintText,
     this.validator,
+    this.maxlines = 1,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      maxLines: maxlines,
       decoration: InputDecoration(
         hintText: hintText,
         hintStyle: const TextStyle(
@@ -33,7 +36,47 @@ class CustomTextFormField extends StatelessWidget {
           borderSide: BorderSide(color: AppColors.primaryDark, width: 2),
         ),
       ),
-      maxLength: 20,
+      validator: validator,
+    );
+  }
+}
+
+class CustomLargeTextFormField extends StatelessWidget {
+
+  final TextEditingController controller;
+  final String hintText;
+  final String? Function(String?)? validator;
+  final int maxlines;
+
+  const CustomLargeTextFormField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.validator,
+    this.maxlines = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      maxLines: maxlines,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: AppColors.textGray,
+          fontSize: 14,
+          fontFamily: 'Inter',
+          fontWeight: FontWeight.w400,
+          height: 0.14,
+        ),
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.textGray),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.primaryDark, width: 2),
+        ),
+      ),
       validator: validator,
     );
   }
