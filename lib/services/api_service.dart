77ed96952a71c8299d8119a7ffa30fa4f1ce6a6a
SongMinhyuk <dnfldpden32@gmail.com>
2024-12-09 19:04:14 +0900

    feat: API service patch문 작성

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -25,6 +25,27 @@ class ApiService {
       throw Exception('Failed to load data: ${body}');
     }
   }
+  /// 주어진 파라미터로 PATCH 전송 + data 필드에 fromJson 매핑하여 반환
+  Future<T> patch<T>(String endpoint, {Map<String, dynamic>? requestBody, Map<String, String>? headers, required T Function(dynamic) fromJson}) async{
+    print("ApiService: PATCH to '$_BASE_URL/$endpoint' with body=$requestBody");
+    headers = {
+      'Content-Type' : 'application/json',
+      ...?_defaultHeader,
+      ...?headers
+    };
+    final response = await http.patch(
+      Uri.parse('$_BASE_URL/$endpoint'),
+      headers: headers,
+      body: requestBody != null ? jsonEncode(requestBody) : null,
+    );
+    String decodedBody = utf8.decode(response.bodyBytes);
+    print("ApiService: PATCH /$endpoint received $decodedBody");
+    if (response.statusCode == 200) {
+      return fromJson(jsonDecode(decodedBody)['data']);
+    } else {
+      throw Exception('Failed to patch data: $decodedBody');
+    }
+  }
 
   /// 주어진 파라미터로 POST 전송 + data 필드에 fromJson 매핑하여 반환
   Future<T> post<T>(String endpoint, {Map<String, dynamic>? requestBody, Map<String, String>? headers, required T Function(dynamic) fromJson}) async {
