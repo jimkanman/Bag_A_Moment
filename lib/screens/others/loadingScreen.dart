b726d85bdca86faaecf725beeb77d3c38ad13bf0
Jaehwan Hyun <jec0101@naver.com>
2024-12-12 23:36:48 +0900

    update: 로딩이미지 복구

--- lib/screens/others/loadingScreen.dart
+++ lib/screens/others/loadingScreen.dart
@@ -1,4 +1,4 @@
-// import 'package:extended_image/extended_image.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //로딩 스크린 클래스 생성
@@ -12,14 +12,14 @@ class LoadingScreen extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           // 전체 화면을 채우는 이미지
-          // Positioned.fill(
-          //   child: ExtendedImage.asset(
-          //     'assets/images/main.png',
-          //     fit: BoxFit.cover,
-          //   ),
-          // ),
+          Positioned.fill(
+            child: ExtendedImage.asset(
+              'assets/images/main.png',
+              fit: BoxFit.cover,
+            ),
+          ),
           // 이미지 위에 중하단에 위치한 로딩 인디케이터
-          Align(
+          const Align(
             alignment: Alignment(0.0, 0.7), // 수직 위치 조정 (1.0이 가장 아래)
             child: CircularProgressIndicator(
               valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFB3EDE5)),
